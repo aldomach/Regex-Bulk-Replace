626338a1001fe1a825e23a915b059a0758b7503f
Aldo <aldomachado@gmail.com>
2025-02-09 16:46:55 -0300

    actualizado todo lo publico

--- compile.bat
+++
@@ -1,2 +0,0 @@
-
-pyinstaller --noconsole --onedir --distpath C:\temp\installer\distpath --workpath C:\temp\installer\build --specpath C:\temp\installer\spec --add-data "C:\x\git\Regex-Bulk-Replace\README.md;." Regex-Bulk-Replace.py
